74403d51fd21fab8491f75bd63cc32bad9e3b160
bhhdev <bhh.develop@gmail.com>
2025-10-09 14:08:39 +0200

    Add: Users controllers

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class CarpoolsController extends AbstractController
+final class SecurityController extends AbstractController
 {
-    #[Route('/covoiturages', name: 'app_carpools')]
+    #[Route('/connexion', name: 'app_security')]
     public function index(): Response
     {
-        return $this->render('carpools/index.html.twig', [
-            'controller_name' => 'Page covoiturages',
+        return $this->render('security/index.html.twig', [
+            'controller_name' => 'Page de connexion',
         ]);
     }
 }
